6357da6d9cba9493fb3474f46f4baadda939a08e
LeviSchouten <levischoutenschool@gmail.com>
2020-06-11 21:21:09 +0200

    styled artists component

--- src/components/Artists.tsx
+++ src/components/Artists.tsx
@@ -1,42 +1,105 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  TableContainer,
+  Paper,
+  IconButton,
+  Box,
+} from "@material-ui/core";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+
+const ARTISTS = gql`
+  query artists($page: Int!) {
+    artists(page: $page) {
+      name
+      bio
+      birthday
+      nationality
+      _id
+    }
+  }
+`;
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 
 const Artists: React.FC = () => {
-  const [page, setPage] = useState(1);
+  const history = useHistory();
 
-  const ARTISTS = gql`
-    query artists($page: Int!) {
-      artists(page: $page) {
-        name
-        _id
-      }
-    }
-  `;
+  const classes = useStyles();
+  const [page, setPage] = useState(1);
 
   const { loading, error, data } = useQuery(ARTISTS, {
     variables: { page },
   });
 
+  const handleRowClick = (id: string) => {
+    history.push("/artists/" + id);
+  };
+
+  interface Artist {
+    [index: string]: string;
+    name: string;
+    _id: string;
+    bio: string;
+    birthday: string;
+    nationality: string;
+  }
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Something went wrong...</p>;
 
-  interface Artist {
-    name: string;
-    _id: string;
-  }
+  const artists: Artist[] = data.artists;
 
   return (
-    <div className="artists">
-      {data.artists.map((artist: Artist) => (
-        <div key={artist._id}>
-          <Link to={`/artists/${artist._id}`}>{artist.name}</Link>
-        </div>
-      ))}
-      <button onClick={() => setPage(page - 1)}>prev</button>
-      <button onClick={() => setPage(page + 1)}>next</button>
-    </div>
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Artists</TableCell>
+            <TableCell>Bio</TableCell>
+            <TableCell>Birthday</TableCell>
+            <TableCell>nationality</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {artists.map((artist: Artist) => {
+            return (
+              <TableRow
+                key={artist._id}
+                hover
+                onClick={() => handleRowClick(artist._id)}
+              >
+                <TableCell>{artist.name || "n/a"}</TableCell>
+                <TableCell>{artist.bio || "n/a"}</TableCell>
+                <TableCell>{artist.birthday || "n/a"}</TableCell>
+                <TableCell>{artist.nationality || "n/a"}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      <Box display="flex" justifyContent="flex-end" m={1}>
+        <IconButton disabled={page === 1} onClick={() => setPage(page - 1)}>
+          <NavigateBeforeIcon />
+        </IconButton>
+        <IconButton onClick={() => setPage(page + 1)}>
+          <NavigateNextIcon />
+        </IconButton>
+      </Box>
+    </TableContainer>
   );
 };
 
