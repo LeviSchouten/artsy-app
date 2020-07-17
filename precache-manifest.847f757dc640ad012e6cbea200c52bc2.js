f1a15fcdc0443ead06d55ac9dc1882428ce50116
LeviSchouten <levischoutenschool@gmail.com>
2020-07-17 13:03:01 +0200

    Updates

---
+++ precache-manifest.847f757dc640ad012e6cbea200c52bc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e961f061e0ab5a3da97531d7ef79eb",
+    "url": "/artsy-app/index.html"
+  },
+  {
+    "revision": "443535e97cfb0daed36e",
+    "url": "/artsy-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "70a91d7c342323ad0f38",
+    "url": "/artsy-app/static/js/2.eac7a1d7.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/artsy-app/static/js/2.eac7a1d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "443535e97cfb0daed36e",
+    "url": "/artsy-app/static/js/main.b84a7e6b.chunk.js"
+  },
+  {
+    "revision": "b085b24ae1537c58fa96",
+    "url": "/artsy-app/static/js/runtime-main.11c0c2b3.js"
+  }
+]);
